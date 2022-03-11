dadfcd044549b93e50c94a26a94a3efbe295f065
Toky RAMANITRA <99540786+Gitoky@users.noreply.github.com>
2022-03-11 21:26:58 +0300

    Update équationDuSecondDegrée.js

--- équationDuSecondDegrée.js
+++ équationDuSecondDegrée.js
@@ -11,7 +11,8 @@ if(A == 0){console.log("X="+ (-C / B));
 }
 
 //Si A=0 et B=0< alors ce n'est pas une équation
-if(A == 0 && B == 0){console.log("Ce n'est pas une équation")
+if(A == 0 && B == 0){
+    console.log("Ce n'est pas une équation")
 }
 
 //Si A!=0 , alors on calcule delta
@@ -23,10 +24,12 @@ let X_1 = (-B + racine_delta) / (2 * A)
 let X_2 = (-B - racine_delta) / (2 * A)
 let X = (-B) / (2 * A)
 
-if(delta > 0){console.log("X="+ X_1 +" ou X="+ X_2);
+if(delta > 0){
+    console.log("X="+ X_1 +" ou X="+ X_2);
 }
 
-else if(delta < 0){console.log("Pas de solution");
+else if(delta < 0){
+    console.log("Pas de solution");
 }
 
 else{
